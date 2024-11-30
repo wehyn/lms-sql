ff004d9e01d57cb38833510e34ce7c8b5b715945
wehyn <wayneegarcia@gmail.com>
2024-11-30 14:24:51 +0800

    added class for a structured system

--- main.cpp
+++ main.cpp
@@ -5,24 +5,13 @@ using namespace std;
 bool createDB();
 bool createTable();
 
-sqlite3 *db;
 
-int main() {
+class Library {
+private:
+    sqlite3 *db;
 
-
-    if (createDB()) {
-        cout << "Successfully created a database";
-    }
-
-    if (createTable()) {
-        cout << "Successfully created a table";
-    }
-
-    sqlite3_close(db);
-    return 0;
-}
-
-bool createDB() {
+public:
+    bool createDB() {
     int res = sqlite3_open("Library.db", &db);
 
     if (res != SQLITE_OK) {
@@ -33,29 +22,49 @@ bool createDB() {
         cout << "Database File creation successful." << endl;
         return true;
     }
-}
-    
-bool createTable() {
-    char *err;
-
-    // Creates a table for book
-    string sql = "CREATE TABLE IF NOT EXISTS BOOK ("
-        "ID INT PRIMARY KEY NOT NULL, "
-        "TITLE TEXT NOT NULL, "
-        "AUTHOR TEXT NOT NULL,"
-        "ISBN INT NOT NULL, "
-        "publicationDATE TEXT NOT NULL,"
-        "availableCopies INT NOT NULL);";
-
-    int res = sqlite3_exec(db, sql.c_str(), NULL, 0, &err);
-
-    if (res != SQLITE_OK) {
-        cout << "Error in creating table: " << err << endl;
-        sqlite3_free(err);
-        return false;
     }
-    else {
-        cout << "Table created successfully." << endl;
-        return true;
+
+    bool createTable() {
+        char *err;
+
+        // Creates a table for book
+        string sql = "CREATE TABLE IF NOT EXISTS BOOK ("
+            "ID INT PRIMARY KEY NOT NULL, "
+            "TITLE TEXT NOT NULL, "
+            "AUTHOR TEXT NOT NULL,"
+            "ISBN INT NOT NULL, "
+            "publicationDATE TEXT NOT NULL,"
+            "availableCopies INT NOT NULL);";
+
+        int res = sqlite3_exec(db, sql.c_str(), NULL, 0, &err);
+
+        if (res != SQLITE_OK) {
+            cout << "Error in creating table: " << err << endl;
+            sqlite3_free(err);
+            return false;
+        }
+        else {
+            cout << "Table created successfully." << endl;
+            return true;
+        }
     }
+
+};
+
+int main() {
+
+    Library lib;
+
+
+    if (lib.createDB()) {
+        cout << "Successfully created a database";
+    }
+
+    if (lib.createTable()) {
+        cout << "Successfully created a table";
+    }
+
+    return 0;
 }
+
+
